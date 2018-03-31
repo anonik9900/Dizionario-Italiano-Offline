f18c80f1431234fb8107041338a19c911988e20a
DeVeL_AnOnIk <impieri.nicholas@gmail.com>
2018-03-31 20:15:51 +0200

    Add files via upload

---
+++ Dizionario Offline  NEW/js/alert.js
@@ -0,0 +1,3 @@
+              alert("Scarica la nuova Applicazione per Windows, Mac Os e Linux");
+              window.open("download.html");
+             alert("Clicca su consenti pop-up per essere riendirizzato alla pagina download ");
